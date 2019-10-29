2a44ba676e4d009c3e1fdad4bc70a6df7ce8ffad
ogihara-ryo <ogihara.ryo.b@gmail.com>
2019-10-29 23:19:39 +0900

    Add navigators

--- App.js
+++ App.js
@@ -1,12 +1,149 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, Platform } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack'
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailScreen from './src/screens/DetailScreen';
+import AddScreen from './src/screens/AddScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import Setting1Screen from './src/screens/Setting1Screen';
+import Setting2Screen from './src/screens/Setting2Screen';
 
 export default function App() {
+  const headerNavigationOptions = {
+    headerStyle: {
+      backgroundColor: 'deepskyblue',
+      marginTop: (Platform.OS === 'android' ? 24 : 0)
+    },
+    headerTitleStyle: { color: 'white' },
+    headerTintColor: 'white',
+  };
+
+  const HomeStack = createStackNavigator({
+    home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        ...headerNavigationOptions,
+        headerTitle: 'Treco',
+        headerBackTitle: 'Home'
+      }
+    },
+    detail: {
+      screen: DetailScreen,
+      navigationOptions: {
+        ...headerNavigationOptions,
+        headerTitle: 'Detail',
+      }
+    }
+  });
+  HomeStack.navigationOptions = ({ navigation }) => {
+    return {
+      tabBarVisible: (navigation.state.index === 0)
+    };
+  };
+  
+  const AddStack = createStackNavigator({
+    add: {
+      screen: AddScreen,
+      navigationOptions: {
+        header: null
+      }
+    }
+  });
+  AddStack.navigationOptions = ({ navigation }) => {
+    return {
+      tabBarVisible: (navigation.state.index === -1)
+    };
+  };
+  
+  const ProfileStack = createStackNavigator({
+    profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        ...headerNavigationOptions,
+        headerTitle: 'Treco',
+        headerBackTitle: 'Profile'
+      }
+    },
+    setting1: {
+      screen: Setting1Screen,
+      navigationOptions: {
+        ...headerNavigationOptions,
+        headerTitle: 'Setting 1',
+      }
+    },
+    setting2: {
+      screen: Setting2Screen,
+      navigationOptions: {
+        ...headerNavigationOptions,
+        headerTitle: 'Setting 2',
+      }
+    }
+  });
+  ProfileStack.navigationOptions = ({ navigation }) => {
+    return {
+      tabBarVisible: (navigation.state.index === 0)
+    };
+  };
+  
+  const MainTab = createBottomTabNavigator(
+    {
+      homeStack: {
+        screen: HomeStack,
+        navigationOptions: {
+          tabBarIcon: ({ tintColor }) => (
+            <Image
+              style={{ height: 25, width: 25, tintColor: tintColor }}
+              source={require('./assets/home.png')}
+            />
+          ),
+          title: 'Home'
+        }
+      },
+      addStack: {
+        screen: AddStack,
+        navigationOptions: {
+          tabBarIcon: () => (
+            <Image
+              style={{ height: 60, width: 60, tintColor: 'deepskyblue' }}
+              source={require('./assets/add.png')}
+            />
+          ),
+          title: '',
+        }
+      },
+      profileStack: {
+        screen: ProfileStack,
+        navigationOptions: {
+          tabBarIcon: ({ tintColor }) => (
+            <Image
+              style={{ height: 25, width: 25, tintColor: tintColor }}
+              source={require('./assets/profile.png')}
+            />
+          ),
+          title: 'Profile'
+        }
+      }
+    },
+    {
+      swipeEnabled: false
+    }
+  );
+
+  const NavigatorTab = createAppContainer(
+    createSwitchNavigator({
+      welcome: { screen: WelcomeScreen },
+      main: { screen: MainTab }
+    })
+  );
+
   return (
     <View style={styles.container}>
-      <WelcomeScreen />
+      <StatusBar barStyle="light-content" />
+      <NavigatorTab />
     </View>
   );
 }
@@ -15,7 +152,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -11,14 +11,7 @@ const SLIDE_DATA = [
 
 class WelcomeScreen extends React.Component {
   onStartButtonPress = () => {
-    Alert.alert(
-      'Alert',
-      'The button was pressed',
-      [
-        { text: 'OK' },
-      ],
-      { cancelable: false }
-    );
+    this.props.navigation.navigate('main');
   }
 
   renderLastButton(index) {
